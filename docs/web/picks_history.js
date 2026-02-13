f82920d6d02f4b50e67d8c042131b0c1febb74fe
bursasearch <bursasearch@gmail.com>
2026-02-13 21:44:23 +0800

    EOD更新 2026-02-13 21:44:23

--- docs/web/picks_history.js
+++ docs/web/picks_history.js
@@ -2409,7 +2409,7 @@ window.allHistoryData = {
   "20260212": {
     "date": "2026-02-12",
     "date_id": "20260212",
-    "last_updated": "2026-02-13 20:48:55",
+    "last_updated": "2026-02-13 21:44:20",
     "source": "20260212_processed_20260213_194156.csv",
     "total_picks": 15,
     "sector_breakdown": {
