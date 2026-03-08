5c58d2cf091e5e235cd38808e004ea0e76e649c4
bursasearch <bursasearch@users.noreply.github.com>
2026-03-08 14:28:19 +0000

    deploy: 08a8bfa4fa1511839daa30c8cbb4858c77910813

---
+++ docs/date_config.js
@@ -0,0 +1,19 @@
+// 自动生成的历史日期列表
+window.availableDates = [
+  {
+    "id": "20260308",
+    "display": "2026-03-08",
+    "file": "history/picks_20260308.json"
+  },
+  {
+    "id": "20260224",
+    "display": "2026-02-24",
+    "file": "history/picks_20260224.json"
+  },
+  {
+    "id": "20260223",
+    "display": "2026-02-23",
+    "file": "history/picks_20260223.json"
+  }
+];
+window.latestDate = "20260308";
