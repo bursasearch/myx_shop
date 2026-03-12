b13d91f1b1af069e66f5076ec7a60f8d64618c09
bursasearch <bursasearch@gmail.com>
2026-03-12 18:40:37 +0800

    每日EOD更新 - 20260312

--- web/date_config.js
+++ web/date_config.js
@@ -1,5 +1,6 @@
 // 自動生成的日期配置文件 - Bursa
 window.availableDates = [
+    {id: '20260312', display: '2026-03-12', file: 'history/picks_20260312.json'},
     {id: '20260311', display: '2026-03-11', file: 'history/picks_20260311.json'},
     {id: '20260310', display: '2026-03-10', file: 'history/picks_20260310.json'},
     {id: '20260309', display: '2026-03-09', file: 'history/picks_20260309.json'},
@@ -7,4 +8,4 @@ window.availableDates = [
     {id: '20260224', display: '2026-02-24', file: 'history/picks_20260224.json'},
     {id: '20260223', display: '2026-02-23', file: 'history/picks_20260223.json'},
 ];
-window.latestDate = "20260311";
+window.latestDate = "20260312";
